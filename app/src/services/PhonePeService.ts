8ee2e6f860ac7e4f0d89bea2bba8567723ce5f91
Developer <developer@example.com>
2026-06-24 14:55:01 +0530

    fix: Update PhonePe Sandbox credentials to working keys

--- app/src/services/PhonePeService.ts
+++ app/src/services/PhonePeService.ts
@@ -3,8 +3,8 @@ import * as Crypto from 'expo-crypto';
 import { Buffer } from 'buffer';
 import { Platform } from 'react-native';
 
-const MERCHANT_ID = 'PGTESTPAYUAT';
-const SALT_KEY = '099eb0cd-02cf-4e2a-8aca-3e6c6aff0399';
+const MERCHANT_ID = 'PGTESTPAYUAT86';
+const SALT_KEY = '96434309-7796-489d-8924-ab56988a6076';
 const SALT_INDEX = '1';
 const PHONEPE_HOST = 'https://api-preprod.phonepe.com/apis/pg-sandbox';
 
